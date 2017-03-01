eacf9ab5248168d366d92dade3368f4ffb8e8fb4
P. Yuan <289683309@qq.com>
2017-03-01 16:07:25 +0800

    Update Bag.java

--- ch1_fundamentals/1.3_p4_Bag/Bag.java
+++ ch1_fundamentals/1.3_p4_Bag/Bag.java
@@ -8,20 +8,7 @@ import edu.princeton.cs.algs4.StdOut;
  *  The {@code Bag} class represents a bag (or multiset) of 
  *  generic items. It supports insertion and iterating over the 
  *  items in arbitrary order.
- *  <p>
- *  This implementation uses a singly-linked list with a static nested class Node.
- *  See {@link LinkedBag} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
+
  *  @param <Item> the generic type of an item in this bag
  */
 public class Bag<Item> implements Iterable<Item> {
